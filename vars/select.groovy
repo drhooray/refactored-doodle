573f0641e28b1eba9b1c9e88d1a78c55b4fa9fe3
Jason Chen <jason.z.chen@intel.com>
2025-03-26 23:21:26 +0800

    [ci] ui for select tag

--- vars/select.groovy
+++ vars/select.groovy
@@ -8,3 +8,26 @@ def call() {
 	//]
 }
 
+def getGitTags() {
+	def repoPath = "ipu7-drivers"
+
+	if (!fileExists(repoPath)) {
+	error "Repository not found at ${repoPath}. Run cloneRepo() first."
+	}
+
+	sh "cd ${repoPath} && git fetch --tags --prune"
+	def output = sh(script: "cd ${repoPath} && git tag", returnStdout: true).trim()
+
+	return output ? output.split('\n') : ['20241206', '20250108_1730_388']
+}
+
+def selectTag(tags) {
+	println "Available tags: ${tags.join(', ')}"
+
+	def selectedTag = tags[-1]
+	println "Selected Tag: ${selectedTag}"
+
+	return selectedTag
+}
+
+return this
